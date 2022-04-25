939e87e10d23d9ef851355c01c3bc3c4c1599854
Em Ha Tuan <hatuanem199801@gmail.com>
2022-04-25 21:46:26 +0700

    [GETTER] Use computed to define a property name.

--- getter/product.js
+++ getter/product.js
@@ -1,11 +1,15 @@
+const seourl = Object.freeze('seourl');
+
 const product = (title, salePrice, oriPrice, description) => ({
     title,
     description,
     get price() {
         return oriPrice - salePrice;
+    },
+    get [seourl]() {
+        return this.title.toLowerCase().replace(/\s/g, "-");
     }
 });
 
-const apple = product('Apple', 200, 300, "This fruit is very heathy.");
-delete apple.title;
-console.log(apple.title);
+const apple = product('Apple from California', 200, 300, "This fruit is very heathy.");
+console.log(apple.seourl);
