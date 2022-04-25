7e9446ca17b162f5b9a7d45caa12ef90e14e24d6
Em Ha Tuan <hatuanem199801@gmail.com>
2022-04-25 21:24:55 +0700

    [GETTER] Use "writable" to allow modify the value of property, default is "true".

--- getter/log.js
+++ getter/log.js
@@ -3,9 +3,10 @@ const log = {
 };
 
 Object.defineProperty(log, 'currentDate', {
-    get() {
-        return new Date();
-    }
+    value: new Date(),
+    writable: false
 })
 
+log.currentDate = "Hallo, May!";
+// TypeError: Cannot assign to read only property 'currentDate' of object '#<Object>'
 console.log(log.currentDate);
