6309d5f44bab9539f5e02331484e272bddf21832
Em Ha Tuan <hatuanem199801@gmail.com>
2022-04-26 20:34:29 +0700

    [SETTER] Use computed to name of property.

--- setter/order.js
+++ setter/order.js
@@ -1,10 +1,23 @@
+const date = Object.freeze('date');
+
 const order = {
     values: [],
+    currentDateOrder: null,
     /**
-     * @param {any} name
+     * @param {string} name
      */
     set current(name) {
         this.values.push(name);
+    },
+
+    
+    /**
+     * set date of order.
+     * @param {Date} value
+     */
+    set [date](value) {
+        // use computed to define name of property.
+        this.currentDateOrder = value;
     }
 }
 
@@ -13,4 +26,7 @@ order.current = 'IN PROGRESS';
 order.current = 'DELIVERY';
 order.current = 'DONE';
 
+order.currentDateOrder = new Date();
+
 console.log(order.values);
+console.log(order.currentDateOrder);
