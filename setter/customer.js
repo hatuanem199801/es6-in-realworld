8488930d2acbcbf2b7c1ed96f221fb8bb49e6498
Em Ha Tuan <hatuanem199801@gmail.com>
2022-04-26 21:18:55 +0700

    [SETTER] Stuck on async await in setter

--- setter/customer.js
+++ setter/customer.js
@@ -1,15 +1,26 @@
+import fetch from 'node-fetch';
+
 const customer = {
     fullName: "",
     firstName: "",
     lastName: "",
+    avatar: ""
 };
 
 Object.defineProperty(customer, 'setFirstName', {
     set: function(value) {
         this.firstName = value.split(" ")[0];
     }
+});
+
+Object.defineProperty(customer, 'getAvatar', {
+    set: async function(value) {
+        this.avatar = await fetch(value).then(res => res.arrayBuffer());
+    }
 })
 
 customer.setFirstName = "Em Ha Tuan";
-
-console.log(customer);
+(async function() {
+    await (customer.getAvatar = "https://picsum.photos/480");
+    console.log(customer);
+})();
